989f9cde52af9f5b04d2b6303e3eb64a5257c207
joaoprds <joaopaulorochadesousa@gmail.com>
2022-07-14 18:58:19 -0300

    begining the copy backend to OLX marketplace

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const Auth = require('./middlewares/Auth')
+
+const AuthValidator = require('./validators/Authvalidator')
+
+const AuthController = require ('./controllers/AuthController');
+const AdsController = require ('./controllers/AdsController');
+const UserController = require ('./controllers/UserController');
+
+
+router.get('/ping', (req, res) => {
+    res.json({pong: true})
+});
+
+router.get('/states', UserController.getStates);
+
+router.post('/user/signin', AuthController.signin);
+router.post('/user/signup', AuthValidator.signup, AuthController.signup);
+
+router.get('/user/me',  Auth.private, UserController.info);
+router.put('/user/me',  Auth.private, UserController.editAction);
+
+router.get('/categories', AdsController.getCategories);
+
+router.post('/ads/add',  Auth.private, AdsController.addAction);
+router.get('/ads/list', AdsController.getList);
+router.get('/ads/item', AdsController.getItem);
+router.post('/ads/:id',  Auth.private, AdsController.editAction);
+
+module.exports = router;
---
+++ src/validators/Authvalidator.js
@@ -0,0 +1,28 @@
+const {checkSchema} = require('express-validator');
+
+module.exports = {
+    signup: checkSchema ({
+        name: {
+            trim: true,
+            isLength: {
+                options: {min: 2}
+            },
+            errorMessage: 'Nome Precisa ter pelo menos 2 caracteres'
+        },
+        email: {
+            isEmail: true,
+            normalizeEmail: true,
+            errorMessage: 'E-mail invalido'
+        },
+        password: {
+            isLength: {
+                options: {min: 6}
+            },
+            errorMessage: 'Senha minimo 6 caracteres'
+        },
+        state: {
+            notEmpty: true,
+            errorMessage: 'Estado não preenchido'
+        }
+    })
+}
